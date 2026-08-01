055bb86e727f40e9135219e6f84acd1eae74ed85
Abdellah Shabat <abzshabat@gmail.com>
2026-08-01 14:54:46 +0300

    feat(splash): create splash screen composable

--- app/src/main/java/com/abdellahshabat/unimate/feature/splash/SplashScreen.kt
+++ app/src/main/java/com/abdellahshabat/unimate/feature/splash/SplashScreen.kt
@@ -1,4 +1,32 @@
 package com.abdellahshabat.unimate.feature.splash
 
-class SplashScreen {
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable //هذه الـ Annotation تخبر Compose أن الدالة مسؤولة عن رسم واجهة المستخدم. لن يعتبر Compose الدالة شاشة أو عنصر واجهة.
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier  //Modifier هو الوسيلة التي نتحكم بها في شكل العنصر وسلوكه.
+
+@Composable
+fun SplashScreen() {
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Text(
+            text = "UniMate AI",
+            style = MaterialTheme.typography.headlineLarge
+        )
+
+        Text(
+            text = "Your Smart University Assistant",
+            style = MaterialTheme.typography.bodyLarge
+        )
+
+    }
 }
