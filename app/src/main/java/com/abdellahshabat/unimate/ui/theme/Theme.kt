eb5d1775652d79ca8bcb73616b9a2555127ebbeb
Abdellah Shabat <abzshabat@gmail.com>
2026-08-01 20:23:36 +0300

    feat(theme): configure Material 3 theme

--- app/src/main/java/com/abdellahshabat/unimate/ui/theme/Theme.kt
+++ app/src/main/java/com/abdellahshabat/unimate/ui/theme/Theme.kt
@@ -1,57 +1,43 @@
 package com.abdellahshabat.unimate.ui.theme
 
-import android.app.Activity
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+//هذا يحدد ألوان التطبيق في الوضع الفاتح.lightColorScheme
+private val LightColors = lightColorScheme(
+    primary = PrimaryBlue,
+    secondary = SecondaryGreen,
+    background = BackgroundLight,
+    surface = SurfaceLight,
+    error = Error
 )
 
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+//darkColorScheme  وهذا يحدد ألوان التطبيق في الوضع الداكن.
+private val DarkColors = darkColorScheme(
+    primary = PrimaryBlueDark,
+    secondary = SecondaryGreen,
+    error = Error
 )
 
+//isSystemInDarkTheme()  يفحص إعدادات الهاتف. إذا كان المستخدم يستخدم الوضع الليلي، سيطبق الألوان الداكنة تلقائيًا.
 @Composable
 fun UniMateTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
 
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+    val colors = if (darkTheme) {
+        DarkColors
+    } else {
+        LightColors
     }
 
+//MaterialTheme  هذا هو قلب تصميم التطبيق.كل عناصر Compose مثل: Button, Text, Card ,NavigationBar ستستخدم هذه الألوان تلقائيًا.
     MaterialTheme(
-        colorScheme = colorScheme,
+        colorScheme = colors,
         typography = Typography,
         content = content
     )
