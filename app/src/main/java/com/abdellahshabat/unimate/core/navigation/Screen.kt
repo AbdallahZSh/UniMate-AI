73c79dcd87fd486524f47d8d7fb9d2a9fcedbde0
Abdellah Shabat <abzshabat@gmail.com>
2026-08-01 20:39:53 +0300

    feat(navigation): add screen routes

---
+++ app/src/main/java/com/abdellahshabat/unimate/core/navigation/Screen.kt
@@ -0,0 +1,21 @@
+package com.abdellahshabat.unimate.core.navigation
+//Route كل شاشة لها اسم.
+//لماذا نستخدم route؟
+//لأن مكتبة Navigation تعتمد على اسم لكل شاشة.
+//مثلاً: Screen.Login.route
+//تعطي :login
+//NavHost هو المكان الذي يجمع جميع الشاشات.
+//NavController  هو المسؤول عن التنقل.
+sealed class Screen(val route: String) {
+
+    data object Splash : Screen("splash")
+
+    data object OnBoarding : Screen("onboarding")
+
+    data object Login : Screen("login")
+
+    data object Register : Screen("register")
+
+    data object Home : Screen("home")
+
+}
