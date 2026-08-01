020610d1e8250786a55709e4a5a0b4767755fd06
Abdellah Shabat <abzshabat@gmail.com>
2026-08-01 20:12:07 +0300

    feat(theme): add application color palette

--- app/src/main/java/com/abdellahshabat/unimate/feature/splash/SplashScreen.kt
+++ app/src/main/java/com/abdellahshabat/unimate/feature/splash/SplashScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.School
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable //هذه الـ Annotation تخبر Compose أن الدالة مسؤولة عن رسم واجهة المستخدم. لن يعتبر Compose الدالة شاشة أو عنصر واجهة.
--- app/src/main/java/com/abdellahshabat/unimate/ui/theme/Color.kt
+++ app/src/main/java/com/abdellahshabat/unimate/ui/theme/Color.kt
@@ -2,10 +2,22 @@ package com.abdellahshabat.unimate.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Primary Colors  PrimaryBlue لأن هذا هو اللون الأساسي الذي سيظهر في الأزرار.شريط التطبيق.العناصر المهمة.
+val PrimaryBlue = Color(0xFF1565C0)
+val PrimaryBlueDark = Color(0xFF0D47A1)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Secondary Colors
+val SecondaryGreen = Color(0xFF43A047)
+
+// Background Colors
+val BackgroundLight = Color(0xFFF5F7FA)
+val SurfaceLight = Color(0xFFFFFFFF)
+
+// Text Colors
+val TextPrimary = Color(0xFF212121)
+val TextSecondary = Color(0xFF757575)
+
+// Status Colors
+val Success = Color(0xFF4CAF50)
+val Warning = Color(0xFFFFA000)
+val Error = Color(0xFFD32F2F)
