60ec6380ef820e14c1b8413a1bb553db5a6dfcdc
Abdellah Shabat <abzshabat@gmail.com>
2026-08-03 00:18:39 +0300

    feat(auth): complete onboarding navigation flow

--- app/src/main/java/com/abdellahshabat/unimate/feature/splash/presentation/SplashViewModel.kt
+++ app/src/main/java/com/abdellahshabat/unimate/feature/splash/presentation/SplashViewModel.kt
@@ -1,10 +1,46 @@
 package com.abdellahshabat.unimate.feature.splash.presentation
-
-import androidx.lifecycle.ViewModel
-
 /**
  * ViewModel responsible for Splash screen logic.
  * يعني أن هذا الكلاس سيعيش مع الشاشة ويحفظ حالتها.
  */
-class SplashViewModel: ViewModel() {
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.abdellahshabat.unimate.core.datastore.PreferenceRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+class SplashViewModel(
+    private val repository: PreferenceRepository
+) : ViewModel() {
+
+    private var _startDestination =
+        MutableStateFlow("")
+
+    val startDestination: StateFlow<String> =
+        _startDestination
+
+    init {
+        checkOnBoardingState()
+    }
+
+    private fun checkOnBoardingState() {
+
+        viewModelScope.launch {
+
+            repository.onboardingCompleted.collect { completed ->
+
+                _startDestination =
+                    if (completed)
+                        MutableStateFlow("login")
+                    else
+                        MutableStateFlow("onboarding")
+
+            }
+
+        }
+
+    }
+
 }
